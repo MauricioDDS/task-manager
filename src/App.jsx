69997511026dbf55799a4023f471552ce634212a
MauricioDDS <mauriciodidonato794@gmail.com>
2025-08-31 09:57:18 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Container, Typography } from "@mui/material";
+
+function App() {
+  return (
+    <Container sx={{ bgcolor: "tomato", height: "100vh" }}>
+      <Typography variant="h1">Hello world!</Typography> 
+    </Container>
+  );
+}
+
+export default App;
